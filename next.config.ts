482fad87b2b37f6a551a1cb7355610d68a9c2827
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2024-10-23 23:43:49 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/your-repo-name',
+  basePath: '/next-on-github-pages',
 };
 
 export default nextConfig;
